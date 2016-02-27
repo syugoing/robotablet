47eac54abfbb75671e5aacc8ac704059452e7cc3
Koji Shiraishi <shiraco@gmail.com>
2016-02-28 00:58:38 +0900

    add tempora;

--- static/js/input.js
+++ static/js/input.js
@@ -21,3 +21,37 @@ new Vue({
     }
   }
 });
+
+var show_menu_template = Vue.extend({
+  template: '' +
+'<div id="app">' +
+'  <div class="row">' +
+'    <div class="col m6 waves-effect waves-light" v-for="btn in btns">' +
+'      <div class="card light-blue darken-1">' +
+'        <div v-on:click="selectButton($index)" class="light-blue darken-1">' +
+'          <div class="card-content">' +
+'            <div class="btn-label-box-r2">' +
+'              <h2 class="truncate left-align white-text btn-label">${ btn.label }</h2>' +
+'            </div>' +
+'          </div>' +
+'        </div>' +
+'      </div>' +
+'    </div>' +
+'  </div>' +
+'</div>'
+});
+
+var show_image_template = Vue.extend({
+  template: '<div>b A custom component!</div>'
+});
+
+new Vue({
+  el: '#container',
+  data: {
+    currentView: 'show-image'
+  },
+  components: {
+    'show-menu': show_menu_template,
+    'show-image': show_image_template
+  }
+});
